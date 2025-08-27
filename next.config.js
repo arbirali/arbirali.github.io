9dcdaf91d8c6a9c878375920530edce1c1695263
arbirali <rahat.ali@arbisoft.com>
2025-08-27 14:22:06 +0500

    Deployment

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 // const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  // output: "export",
+  output: "export",
   // basePath: isProd ? "/arbirali.github.io" : "",
   // assetPrefix: isProd ? "/arbirali.github.io/" : "",
   images: {
